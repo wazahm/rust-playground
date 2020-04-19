2071207711d70bf615eed90eac600e931ceaa740
Wazir Ahmed <wazirahmedf@gmail.com>
2020-04-19 15:11:26 +0000

    Implemented HttpServer::parse_request() and added HttpResponse struct

--- http-server/src/http_server.rs
+++ http-server/src/http_server.rs
@@ -2,12 +2,17 @@
 
 use std::collections::HashMap;
 use std::net:: { TcpListener, TcpStream };
-use std::thread;
 use std::io;
+use std::io::Read;
+use std::thread;
+use std::error::Error;
 use std::sync::Arc;
 use std::ops::Deref;
 
-type HttpRequestHandler = fn(HashMap<String, String>, Vec<u8>);
+const CRLF: &str = "\r\n";
+const DOUBLE_CRLF_ASCII: [u8; 4] = ['\r' as u8, '\n' as u8, '\r' as u8, '\n' as u8];
+
+type HttpRequestHandler = fn(HttpRequest, HttpResponse);
 
 #[derive(PartialEq, Clone)]
 enum HttpMethod {
@@ -18,10 +23,19 @@ enum HttpMethod {
     NONE
 }
 
-struct HttpRequest {
-    socket: TcpStream,
-    header: HashMap<String, String>,
-    body: Vec<u8>
+pub struct HttpRequest {
+    pub header: HashMap<String, String>,
+    pub body: Vec<u8>
+}
+
+pub struct HttpResponse {
+    socket: TcpStream
+}
+
+impl HttpResponse {
+    pub fn new(socket: TcpStream) -> HttpResponse {
+        HttpResponse { socket: socket }
+    }
 }
 
 #[derive(Clone)]
@@ -39,27 +53,102 @@ impl HttpServer {
     pub fn new() -> HttpServer {
         HttpServer { endpoints: Vec::new() }
     }
-    fn add(&mut self, url: String, method: HttpMethod, cb: HttpRequestHandler) {
+    fn add(&mut self, url: &str, method: HttpMethod, cb: HttpRequestHandler) {
         self.endpoints.push(HttpEndpoint {
-            url: url,
+            url: String::from(url),
             method: method,
             callback: cb
         });
     }
-    pub fn get(&mut self, url: &String, callback: fn(HashMap<String, String>, Vec<u8>)) {
-        self.add(url.clone(), HttpMethod::GET, callback);
+    pub fn get(&mut self, url: &str, callback: HttpRequestHandler) {
+        self.add(url, HttpMethod::GET, callback);
     }
-    pub fn post(&mut self, url: &String, callback: fn(HashMap<String, String>, Vec<u8>)) {
-        self.add(url.clone(), HttpMethod::POST, callback);
+    pub fn post(&mut self, url: &str, callback: HttpRequestHandler) {
+        self.add(url, HttpMethod::POST, callback);
     }
-    pub fn put(&mut self, url: &String, callback: fn(HashMap<String, String>, Vec<u8>)) {
-        self.add(url.clone(), HttpMethod::PUT, callback);
+    pub fn put(&mut self, url: &str, callback: HttpRequestHandler) {
+        self.add(url, HttpMethod::PUT, callback);
     }
-    pub fn delete(&mut self, url: &String, callback: fn(HashMap<String, String>, Vec<u8>)) {
-        self.add(url.clone(), HttpMethod::DELETE, callback);
+    pub fn delete(&mut self, url: &str, callback: HttpRequestHandler) {
+        self.add(url, HttpMethod::DELETE, callback);
     }
-    fn parse_request(stream: TcpStream) -> HttpRequest {
-        HttpRequest { socket: stream, header: HashMap::new(), body: Vec::new() }
+    fn parse_request(stream: &mut TcpStream) -> Result<HttpRequest, Box<dyn Error>> {
+        let mut header_buffer: Vec<u8> = Vec::new();
+        let mut header_read = false;
+        let stream_ref = stream.by_ref();
+        //let stream_ref = Read::by_ref(&mut stream);
+        for _byte in stream_ref.bytes() {
+            let byte = _byte?;
+            header_buffer.push(byte);
+            if header_buffer.ends_with(&DOUBLE_CRLF_ASCII) == true {
+                header_read = true;
+                break;
+            }
+        }
+
+        if header_read == false {
+            // stream ended; connection closed. But not received the complete HTTP header
+            let custom_err = io::Error::new(io::ErrorKind::UnexpectedEof, "Incomplete HTTP header");
+            return Result::Err(Box::new(custom_err));
+        }
+
+        let header = String::from_utf8(header_buffer)?;
+
+        let mut header_map: HashMap<String, String> = HashMap::new();
+
+        for (i, line) in header.split(CRLF).enumerate() {
+            if i == 0 {
+                // Parse the first line => GET /url HTTP/1.1
+                let words: Vec<&str> = line.split(" ").collect();
+
+                if words.len() != 3 {
+                    let custom_err = io::Error::new(io::ErrorKind::InvalidData, "Invalid HTTP header");
+                    return Result::Err(Box::new(custom_err));
+                }
+
+                // TODO: Validate before inserting items in the hash map
+                header_map.insert(String::from("method"), String::from(words[0]));
+                header_map.insert(String::from("url"), String::from(words[1]));
+                header_map.insert(String::from("http-version"), String::from(words[2].trim_start_matches("HTTP/")));
+            } else {
+                let field_value: Vec<&str> = line.split(":").map(|x| x.trim()).collect();
+                if field_value.len() != 2 {
+                    continue;
+                } else {
+                    // TODO: Deal with the HTTP fields which has multiple values or key-value pairs within the value part
+                    header_map.insert(field_value[0].to_lowercase(), String::from(field_value[1]));
+                }
+            }
+        }
+
+        let mut content_length = 0;
+        match header_map.get("content-length") {
+            Some(x) => {
+                match x.parse::<u32>() {
+                    Ok(x) => {
+                        content_length = x;
+                    },
+                    Err(error) => {
+                        eprintln!("Error: {:?}", error);
+                    }
+                }
+            },
+            None => {}
+        }
+
+        let mut body: Vec<u8> = Vec::new();
+        if content_length > 0 {
+            for _byte in stream_ref.bytes() {
+                let byte = _byte?;
+                body.push(byte);
+                content_length -= 1;
+                if content_length == 0 {
+                    break;
+                }
+            }
+        }
+
+        Result::Ok(HttpRequest { header: header_map, body: body })
     }
     fn get_request_handler(endpoints: &Vec<HttpEndpoint>, url: &String, method: HttpMethod) -> Option<HttpRequestHandler> {
         for endpoint in endpoints {
@@ -75,10 +164,20 @@ impl HttpServer {
 
         /* TcpListener::incoming() does accept() & returns the Result<TcpStream> */
         for conn in listener.incoming() {
-            let stream = conn?;
+            let mut stream = conn?;
             let endpoints_ref = endpoints.clone();
             thread::spawn(move || {
-                let request = HttpServer::parse_request(stream);
+                let request: HttpRequest;
+                match HttpServer::parse_request(&mut stream) {
+                    Ok(req) => {
+                        request = req;
+                    },
+                    Err(error) => {
+                        eprintln!("Error: {:?}", error);
+                        return;
+                    }
+                }
+
                 let method: HttpMethod;
                 match request.header.get("method") {
                     Some(x) => { 
@@ -107,7 +206,7 @@ impl HttpServer {
                 }
                 match HttpServer::get_request_handler(endpoints_ref.deref(), url, method) {
                     Some(cb) => {
-                        cb(request.header, request.body);
+                        cb(request, HttpResponse::new(stream));
                     }
                     None => {
                         // TODO: Send the right error code.
--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -1,184 +1,39 @@
 mod http_server;
 
-use std::ops::Add;
-use std::io::{Read, Write};
-use std::net::{TcpStream, TcpListener};
-use std::thread;
-use std::collections::HashMap;
+use http_server::HttpServer;
+use serde::{Serialize, Deserialize};
+use serde_json::Result as SerdeResult;
 
 const SERVER_IP: &str = "127.0.0.1";
 const SERVER_PORT: u16 = 8080;
 
-const CRLF: &str = "\r\n";
-const DOUBLE_CRLF_ASCII: [u8; 4] = ['\r' as u8, '\n' as u8, '\r' as u8, '\n' as u8];
-
-/*
-struct HttpRequest {
-    header: HashMap<String, String>,
-    body: Vec<u8>
+#[derive(Deserialize, Debug)]
+struct HelloRequest {
+    name: String
 }
 
-
-fn parse_request(stream: &TcpStream) -> HttpRequest {
-
-}
-*/
-
-fn handle_conn(mut stream: TcpStream) {
-    let mut header_buffer: Vec<u8> = Vec::new();
-    let mut header_read = false;
-    let stream_ref = Read::by_ref(&mut stream);
-    for byte in stream_ref.bytes() {
-        match byte {
-            Ok(x) => {
-                header_buffer.push(x);
-                if header_buffer.ends_with(&DOUBLE_CRLF_ASCII) == true {
-                    header_read = true;
-                    break;
-                }
-            },
-            Err(error) => {
-                eprintln!("Error: {:?}", error);
-                return;
-            }
-        }
-    }
-
-    if header_read == false {
-        // stream ended. But not received the complete HTTP header
-        return;
-    }
-
-    let header: String;
-
-    match String::from_utf8(header_buffer) {
-        Ok(x) => {
-            header = x;
-        },
-        Err(error) => {
-            eprintln!("Error: {:?}", error);
-            return;
-        }
-    }
-
-    let mut header_map: HashMap<String, String> = HashMap::new();
-
-    for (i, line) in header.split(CRLF).enumerate() {
-        if i == 0 {
-            // Parse the first line => GET /url HTTP/1.1
-            let words: Vec<&str> = line.split(" ").collect();
-
-            if words.len() != 3 {
-                return;
-            }
-
-            // TODO: Validate before inserting items in the hash map
-            header_map.insert(String::from("method"), String::from(words[0]));
-            header_map.insert(String::from("url"), String::from(words[1]));
-            header_map.insert(String::from("http-version"), String::from(words[2].trim_start_matches("HTTP/")));
-        } else {
-            let field_value: Vec<&str> = line.split(":").map(|x| x.trim()).collect();
-            if field_value.len() != 2 {
-                continue;
-            } else {
-                // TODO: Deal with the HTTP fields which has multiple values or key-value pairs within the value part
-                header_map.insert(field_value[0].to_lowercase(), String::from(field_value[1]));
-            }
-        }
-    }
-
-    let mut content_length = 0;
-    match header_map.get("content-length") {
-        Some(x) => {
-            match x.parse::<u32>() {
-                Ok(x) => {
-                    content_length = x;
-                },
-                Err(error) => {
-                    eprintln!("Error: {:?}", error);
-                }
-            }
-        },
-        None => {}
-    }
-
-    let mut body_buffer: Vec<u8> = Vec::new();
-    if content_length > 0 {
-        for byte in stream_ref.bytes() {
-            match byte {
-                Ok(x) => {
-                    body_buffer.push(x);
-                    content_length -= 1;
-                    if content_length == 0 {
-                        break;
-                    }
-                },
-                Err(error) => {
-                    eprintln!("Error: {:?}", error);
-                    return;
-                }
-            }
-        }
-    }
-
-    let mut body = String::new();
-    match String::from_utf8(body_buffer) {
-        Ok(x) => {
-            body += &x;
-        },
-        Err(error) => {
-            eprintln!("Error: {:?}", error);
-        }
-    }
-
-    println!("\n--------------HTTP Request---------");
-    println!("{:?}\n", header_map);
-    println!("{}", body);
-    println!("--------------END---------\n");
-
-    // TODO: Change these based on the endpoint
-    let response_body = "Hi";
-    let response_length = response_body.len().to_string();
-    let content_type = "text/plain";
-
-    let mut response = String::new();
-    response = response.add("HTTP/1.1 200 OK").add(CRLF);
-    response = response.add("Connection: close").add(CRLF);
-    response = response.add("Content-Type: ").add(&content_type).add(CRLF);
-    response = response.add("Content-Length: ").add(&response_length).add(CRLF).add(CRLF);
-    response = response.add(response_body);
-
-    println!("\n--------------HTTP Response---------");
-    println!("{}", response);
-    println!("--------------END---------\n");
-
-    let response_bytes: Vec<u8> = response.chars().map(|x| x as u8).collect();
-    match stream.write(&response_bytes) {
-        Err(error) => println!("Error: {:?}", error),
-        _ => {}
-    }
+#[derive(Serialize, Debug)]
+struct HelloResponse {
+    message: String
 }
 
 fn main() {
-    let listener = match TcpListener::bind((SERVER_IP, SERVER_PORT)) {
-        Ok(x) => x,
-        Err(error) => {
-            eprintln!("Error: {:?}", error);
-            std::process::exit(0)
-        },
-    };
+    let mut server = HttpServer::new();
 
-    /* TcpListener::incoming() does accept() & returns the Result<TcpStream> */
-    for conn in listener.incoming() {
-        match conn {
-            Ok(stream) => {
-                thread::spawn(move || {
-                    handle_conn(stream);
-                });
+    server.post("/hello", | request, response | {
+        let json_body: SerdeResult<HelloRequest> = serde_json::from_slice(&request.body);
+        match json_body {
+            Ok(hello_req) => {
+                let hello_res = HelloResponse { message: String::from("Hi, ") + &hello_req.name + "!" };
+                let data = serde_json::to_string(&hello_res).unwrap();
+                // response.send(&data);
             },
             Err(error) => {
-                eprintln!("Error: {:?}", error);
-            },
-        };
-    }
+                let data = error.to_string();
+                // response.send(&data);
+            }
+        }
+    });
+
+    server.run(SERVER_IP, SERVER_PORT);
 }
